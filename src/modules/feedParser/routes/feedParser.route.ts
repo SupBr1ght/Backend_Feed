d1f758b38b6d01adfe78e0871c2654e23e63189c
sap <pechkursasha@gmail.com>
2025-09-20 14:47:54 +0300

    feat: Add RSS feed parser and save feeds to MongoDB via Prisma

--- src/modules/feedParser/routes/feedParser.route.ts
+++ src/modules/feedParser/routes/feedParser.route.ts
@@ -1,5 +1,6 @@
 import type { JsonSchemaToTsProvider } from "@fastify/type-provider-json-schema-to-ts";
 import type { FastifyInstance } from "fastify";
+import Parser from "rss-parser";
 import { schema } from "../schemas/getFeedData.schema";
 
 /**
@@ -9,13 +10,68 @@ import { schema } from "../schemas/getFeedData.schema";
 export async function getFeedDataRoutes(fastify: FastifyInstance) {
 	const route = fastify.withTypeProvider<JsonSchemaToTsProvider>();
 
+	const parser: Parser = new Parser();
+
 	route.get(
 		"/feed",
 		{
 			schema: schema,
 		},
 		async (request, reply) => {
-			reply.send({ hello: "feed" });
+			// enpoint with url and force flag
+			const { url, force } = request.query as { url: string; force?: boolean };
+
+			if (!url) {
+				return reply
+					.status(400)
+					.send({ error: 'Query param "url" is required' });
+			}
+
+			try {
+				const feed = await parser.parseURL(url);
+
+				const savedItems = await Promise.all(
+					feed.items.map(async (item) => {
+						const link = item.link || "";
+						if (!link) return null;
+
+						const existing = await fastify.prisma.rssFeed.findUnique({
+							where: { link },
+						});
+
+						if (!existing) {
+							return await fastify.prisma.rssFeed.create({
+								data: {
+									title: item.title || "",
+									link,
+									content: item.contentSnippet || item.content || "",
+									image: item.enclosure?.url || null,
+								},
+							});
+						} else if (force) {
+							return await fastify.prisma.rssFeed.update({
+								where: { link },
+								data: {
+									title: item.title || "",
+									content: item.contentSnippet || item.content || "",
+									image: item.enclosure?.url || null,
+								},
+							});
+						} else {
+							return existing;
+						}
+					}),
+				);
+
+				const filteredItems = savedItems.filter(Boolean);
+
+				return {
+					feedTitle: feed.title,
+					items: filteredItems,
+				};
+			} catch (error) {
+				return reply.status(500).send({ error: error.message });
+			}
 		},
 	);
 }
