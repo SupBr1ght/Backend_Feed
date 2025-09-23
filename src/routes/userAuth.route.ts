618d79c47cc8f0eb303fc660f86636d11d4c98df
sap <pechkursasha@gmail.com>
2025-09-23 14:08:16 +0300

    feat: create user auth

---
+++ src/routes/userAuth.route.ts
@@ -0,0 +1,32 @@
+import type { JsonSchemaToTsProvider } from "@fastify/type-provider-json-schema-to-ts";
+import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { createUserSchema } from "../modules/auth/schemas/userScheme.scheme";
+import { loginUser } from "../modules/auth/services/loginUser.service";
+
+const defaultRoute = async (fastify: FastifyInstance) => {
+    const route = fastify.withTypeProvider<JsonSchemaToTsProvider>();
+
+    route.post(
+        "/login",
+        {
+            schema: createUserSchema,
+        },
+        async (request: FastifyRequest, reply: FastifyReply) => {
+            // enpoint with url and force flag
+            const { email, password } = request.body as {
+                email: string;
+                password: string;
+            };
+            try {
+                // pass Prisma client to keep the service logic separate
+                const { registerUser } = await loginUser(fastify.prisma, email, password);
+                const user = await registerUser(fastify);
+                return reply.status(201).send({ email: user.email, id: user.id });
+            } catch (error) {
+                return reply.status(500).send({ error: error.message })
+            }
+        },
+    );
+};
+
+export default defaultRoute;
